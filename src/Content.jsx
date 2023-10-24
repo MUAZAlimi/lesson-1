abb8e40bcd72fc326f98a50c0ce78c4dc5e2f87c
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-24 15:29:02 +0100

    update

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,13 +2,27 @@
 
 const Content = () => {
      const handleNameChange = () => {
-     const name = ["Ali", "Dave", "Brisa"]
+     const names = ["Muaz", "Dave", "Brisa"]
      const int =Math.floor(Math.random() * 3)
-     return name[int]
+     return names[int]
+    }
+    const handleClick = () => {
+        console.log("you click it");
+    }
+    const handleClickOne = (names) => {
+        console.log(`${names} was click`)
+    } 
+    const handleClickTwo = (e) => {
+        console.log(e.target.innerText)
     }
     return (
         <main>
-        Hello Mr {handleNameChange()}
+            <p> 
+                Hello Mr {handleNameChange()} 💯🤑🎊😎
+            </p>
+            <button onClick={handleClick} >  CLICK  </button><hr />
+            <button onClick={() => handleClickOne("Muaz")} >CLICK NAME</button><hr />
+            <button onClick={(e) => handleClickTwo(e)}>CLICK NOW</button>
         </main>
     )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,12 +4,13 @@ const Header = () => {
   const headerStyle = {
     backgroundColor: "aqua",
     color: "black",
-    borderRadius: "3px",
+    borderRadius: "10px",
     padding: "2px"
   }
   return (
     <header style={headerStyle}>
       <h1>WELCOME TO REACT IN DETAILS</h1>
+      <h2 style={{color: "aqua", fontSize: "30px", borderTopRightRadius: "5px", backgroundColor: "#081b29"}}>WELCOME TO DLT-AFRICA BOOT CAMP</h2>
     </header>
   )
 }
